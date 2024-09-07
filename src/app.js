001ae2a7e0df19c98fc6c9576dcd21d5e9f90815
Nahuel <nahuelopezm@gmail.com>
2024-09-07 19:35:06 -0300

    Primer commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from'express';
+
+//Importar los routes
+
+import productRouter from './routes/products.router.js';
+import cartRouter from './routes/carts.router.js';
+
+
+const app = express();
+
+//Inicializar el servidor
+app.listen(8080, () => {
+    console.log("El servidor se encuentra escuchando");
+})
+
+// Middleware para analizar el cuerpo de las solicitudes
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+//Implementar los routers 
+app.use('/api/products', productRouter);
+app.use('/api/cart', cartRouter);
